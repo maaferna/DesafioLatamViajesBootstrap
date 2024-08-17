450d81062c2604f5e848d0f58ee700c8e472520f
mparraf <maaferna@gmail.com>
2024-08-16 21:23:09 -0400

    Se implementa el tamaño header con carrousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
     tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
+    const header = document.querySelector('.header');
+    const mainContent = document.querySelector('.main-content');
     
+    if (header && mainContent) {
+        const headerHeight = header.offsetHeight;
+        mainContent.style.marginTop = `${headerHeight + 48}px`;
+    }
 });
 
 
